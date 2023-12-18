468a6393ba09a185e31b97761d27504a73e83923
zielvna <zielvna@gmail.com>
2023-12-18 07:06:27 +0100

    add tests to route

--- src/structs/route.rs
+++ src/structs/route.rs
@@ -32,3 +32,49 @@ impl Route {
         self.action.run(request)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        cell::Cell,
+        io::Write,
+        net::{TcpListener, TcpStream},
+        rc::Rc,
+        thread,
+    };
+
+    fn request_from_bytes(data: &[u8]) -> Result<Request, SerwerError> {
+        let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+        let address = listener.local_addr().unwrap();
+
+        let buf = data.to_owned();
+
+        thread::spawn(move || {
+            let mut stream = TcpStream::connect(address).unwrap();
+            stream.write_all(&buf).unwrap();
+        });
+
+        let (stream, _) = listener.accept().unwrap();
+        Request::from_stream(&stream)
+    }
+
+    #[test]
+    fn test_new_closure_run() {
+        let count = Rc::new(Cell::new(0));
+        let count_clone = Rc::clone(&count);
+
+        let route = Route::new(Method::GET, "/", move |_, res| {
+            count_clone.set(count_clone.get() + 1);
+            res
+        })
+        .unwrap();
+
+        let request = request_from_bytes(b"GET / HTTP/1.1\r\n\r\n").unwrap();
+        route.run_action(request.clone());
+        route.run_action(request.clone());
+        route.run_action(request);
+
+        assert_eq!(count.get(), 3);
+    }
+}
