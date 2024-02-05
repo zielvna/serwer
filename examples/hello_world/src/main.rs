116d03b09b4e0518706bf6141a3315d10c0407e6
zielvna <zielvna@gmail.com>
2024-02-05 04:09:18 +0100

    refactor hello world example

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,12 +1,15 @@
-use serwer::{Serwer, StatusCode};
+use serwer::{route, Serwer, StatusCode};
 
 fn main() {
     let mut serwer = Serwer::new();
 
-    serwer.get("/", |_, mut res| {
-        res.set(StatusCode::OK, "Hello world".to_string());
-        res
-    });
+    serwer.get(
+        "/",
+        route! {() move |_, mut res| {
+            res.set(StatusCode::OK, "Hello world".to_string());
+            res
+        }},
+    );
 
     serwer.listen(7878);
 }
